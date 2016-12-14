573eda4a74f2eaecf5287acbe86129282fb12745
Joe Schmitt <joe@compass.com>
2016-12-14 17:25:41 -0500

    Fix angular version and remove bindings for now.

--- src/angularComponent/ngcOmniboxComponent.js
+++ src/angularComponent/ngcOmniboxComponent.js
@@ -1,16 +1,6 @@
 import NgcOmniboxController from './ngcOmniboxController.js';
 
 export default {
-  bindings: {
-    ngModel: '<',
-    ngDisabled: '&',
-    multiple: '<',
-    placeholder: '<',
-    accessoryText: '<',
-    autofocus: '<',
-    onShowSuggestions: '&',
-    onHideSuggestions: '&'
-  },
   controller: NgcOmniboxController,
   controllerAs: 'omnibox'
 };
