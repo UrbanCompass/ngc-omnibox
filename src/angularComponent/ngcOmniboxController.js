2621eaf2b9d2db3ddcaf5c085cf69f7b3c194aab
Joe Schmitt <joe@compass.com>
2017-01-10 14:42:11 -0500

    Remove erroneous function

--- src/angularComponent/ngcOmniboxController.js
+++ src/angularComponent/ngcOmniboxController.js
@@ -110,10 +110,6 @@ export default class NgcOmniboxController {
     return (this.isLoading || this.query) && this.canShow({query: this.query}) !== false;
   }
 
-  shouldShowChoices() {
-    return this.multple && Array.isArray(this.ngModel) && this.ngModel.length;
-  }
-
   /**
    * Tries to update the index of the currently highlighted item to be the next item in the list. If
    * we've reached the end, it'll loop back around and highlight the first item.
