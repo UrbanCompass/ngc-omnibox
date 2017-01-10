20838cb0a5457e9c59439c1f4c59412fb216ee07
Joe Schmitt <joe@compass.com>
2017-01-10 17:03:13 -0500

    Allow falsy values for suggestions.
    This allows you to pass null if you just want to hide the suggestions.

--- src/angularComponent/ngcOmniboxController.js
+++ src/angularComponent/ngcOmniboxController.js
@@ -279,7 +279,7 @@ export default class NgcOmniboxController {
 
       this._hideLoading();
 
-      if (Array.isArray(suggestions)) {
+      if (suggestions && Array.isArray(suggestions)) {
         this.suggestions = suggestions;
       } else {
         throw new Error('Suggestions must be an Array');
