f72aeec33fff7c60c5628baefbbdc9cc52b54c18
Joe Schmitt <joe@compass.com>
2017-01-11 16:04:58 -0500

    Add lint rules for example folder and fix lint errors

--- examples/category-results.js
+++ examples/category-results.js
@@ -1,74 +1,76 @@
-angular
-  .module('demoApp', ['ngc.omnibox'])
-  .config(function($sceDelegateProvider) {
-    $sceDelegateProvider.resourceUrlWhitelist([
-      'self',
-      'http://www.carqueryapi.com/api/0.3/**'
-    ]);
-  })
-  .controller('OmniboxExampleController', function ($http, $q, $scope) {
-    var demo = this;
-    var fuse;
+(function (angular, Fuse) {
+  angular
+    .module('demoApp', ['ngc.omnibox'])
+    .config(function ($sceDelegateProvider) {
+      $sceDelegateProvider.resourceUrlWhitelist([
+        'self',
+        'http://www.carqueryapi.com/api/0.3/**'
+      ]);
+    })
+    .controller('OmniboxExampleController', function ($http, $q) {
+      var demo = this;
+      var fuse;
 
-    // Loads the remote data and populates the search engine
-    function populateSearch() {
-      return $q(function (resolve) {
-        if (fuse) {
-          resolve(fuse);
-        } else {
-          $http.jsonp('http://www.carqueryapi.com/api/0.3/?cmd=getMakes', {
-            jsonpCallbackParam: 'callback'
-          })
-          .then(function (response) {
-            fuse = new Fuse(response.data.Makes, {keys: ['make_display'], threshold: 0.3});
+      // Loads the remote data and populates the search engine
+      function populateSearch() {
+        return $q(function (resolve) {
+          if (fuse) {
             resolve(fuse);
-          });
-        }
-      });
-    };
-
-    // Takes results from JSON API and categorizes the results by `make_country`
-    function formatResults(results) {
-      const grouped = {};
-
-      // Populate groups with results
-      results.forEach((result) => {
-        const groupName = result.make_country;
-        if (!grouped[groupName]) {
-          grouped[groupName] = {
-            make_country: result.make_country,
-            children: []
-          };
-        }
-
-        grouped[groupName].children.push(result);
-      });
-
-      return Object.keys(grouped).map((groupName) => grouped[groupName]);
-    };
-
-    this.model = [];
-
-    // Only allow items, and not category headers, to be selectable
-    this.isSelectable = function (item) {
-      return !item.children;
-    };
-
-    // Only show suggestions when at least 2 characters have been entered
-    this.shouldShowSuggestions = function (query) {
-      return query.length >= 2;
-    };
-
-    this.sourceFn = function (query) {
-      if (query) {
-        return populateSearch().then(function (fuse) {
-          const searchResults = fuse.search(query).filter(function (result) {
-            return !demo.model.includes(result);
-          });
-          return $q.resolve(formatResults(searchResults));
+          } else {
+            $http.jsonp('http://www.carqueryapi.com/api/0.3/?cmd=getMakes', {
+              jsonpCallbackParam: 'callback'
+            })
+            .then(function (response) {
+              fuse = new Fuse(response.data.Makes, {keys: ['make_display'], threshold: 0.3});
+              resolve(fuse);
+            });
+          }
         });
-      } else {
-        return $q.resolve(); // Hides the suggestions
       }
-    };
-  });
+
+      // Takes results from JSON API and categorizes the results by `make_country`
+      function formatResults(results) {
+        const grouped = {};
+
+        // Populate groups with results
+        results.forEach((result) => {
+          const groupName = result.make_country;
+          if (!grouped[groupName]) {
+            grouped[groupName] = {
+              make_country: result.make_country,
+              children: []
+            };
+          }
+
+          grouped[groupName].children.push(result);
+        });
+
+        return Object.keys(grouped).map((groupName) => grouped[groupName]);
+      }
+
+      this.model = [];
+
+      // Only allow items, and not category headers, to be selectable
+      this.isSelectable = function (item) {
+        return !item.children;
+      };
+
+      // Only show suggestions when at least 2 characters have been entered
+      this.shouldShowSuggestions = function (query) {
+        return query.length >= 2;
+      };
+
+      this.sourceFn = function (query) {
+        if (query) {
+          return populateSearch().then(function (fuse) {
+            const searchResults = fuse.search(query).filter(function (result) {
+              return !demo.model.includes(result);
+            });
+            return $q.resolve(formatResults(searchResults));
+          });
+        } else {
+          return $q.resolve(); // Hides the suggestions
+        }
+      };
+    });
+})(window.angular, window.Fuse);
--- examples/simple-suggest.js
+++ examples/simple-suggest.js
@@ -1,39 +1,41 @@
-angular
-  .module('demoApp', ['ngc.omnibox'])
-  .controller('OmniboxExampleController', function ($http, $q) {
-    var fuse;
+(function (angular, Fuse) {
+  angular
+    .module('demoApp', ['ngc.omnibox'])
+    .controller('OmniboxExampleController', function ($http, $q) {
+      var fuse;
 
-    // Loads the remote data and populates the search engine
-    function populateSearch() {
-      return $q(function (resolve) {
-        if (fuse) {
-          resolve(fuse);
-        } else {
-          $http.get('https://api.github.com/emojis').then(function (response) {
-            var emoji = Object.keys(response.data).map(function (id) {
-              return {
-                id,
-                url: response.data[id]
-              }
-            });
-
-            fuse = new Fuse(emoji, {keys: ['id'], threshold: 0.3});
+      // Loads the remote data and populates the search engine
+      function populateSearch() {
+        return $q(function (resolve) {
+          if (fuse) {
             resolve(fuse);
-          });
-        }
-      });
-    };
+          } else {
+            $http.get('https://api.github.com/emojis').then(function (response) {
+              var emoji = Object.keys(response.data).map(function (id) {
+                return {
+                  id,
+                  url: response.data[id]
+                };
+              });
 
-    this.model = '';
+              fuse = new Fuse(emoji, {keys: ['id'], threshold: 0.3});
+              resolve(fuse);
+            });
+          }
+        });
+      }
 
-    this.sourceFn = function (query) {
-      return populateSearch().then(function (fuse) {
-        if (query) {
-          var results = fuse.search(query);
-          return $q.resolve(results);
-        } else {
-          return $q.resolve(); // Hides the suggestions
-        }
-      });
-    };
-  });
+      this.model = '';
+
+      this.sourceFn = function (query) {
+        return populateSearch().then(function (fuse) {
+          if (query) {
+            var results = fuse.search(query);
+            return $q.resolve(results);
+          } else {
+            return $q.resolve(); // Hides the suggestions
+          }
+        });
+      };
+    });
+})(window.angular, window.Fuse);
