f4060b54bfe44f1a09ed854e58203162b74dcba5
Joe Schmitt <joe@compass.com>
2017-01-17 17:52:26 -0500

    Add documentation for field directive

--- src/angularComponent/ngcOmniboxFieldDirective.js
+++ src/angularComponent/ngcOmniboxFieldDirective.js
@@ -1,3 +1,23 @@
+/**
+ * Omnibox Field Directive
+ *
+ * The Field Directive is used to determine where in the Omnibox Component to render the input field
+ * element. It can be used either as a custom element, or an attribute on a standard HTML element.
+ * Once included, an input field element will be appended inside and used for all typing events.
+ * If you need access to the actual input field used (or want it to be rendered on the page before
+ * Angular is init'd), then you can include an input element inside this directive and that will
+ * be used instead of a new one being created.
+ *
+ * This directive also has the following bindings available:
+ *
+ * - ngFocus(): This callback is executed when the input field receives focus.
+ * - ngBlur(): This callback is executed when the input field loses focus.
+ *
+ * Sample usage:
+ *     <ngc-omnibox source="myController.getSuggestions(query)" ng-model="myController.choices">
+ *       <ngc-omnibox-field><input type="text" placeholder="Search here..."></ngc-omnibox-field>
+ *     </ngc-omnibox>
+ */
 ngcOmniboxFieldDirective.$inject = ['$document'];
 export default function ngcOmniboxFieldDirective($document) {
   return {
