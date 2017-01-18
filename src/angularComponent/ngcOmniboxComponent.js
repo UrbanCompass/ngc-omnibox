c23b76dcc5108449e9f1d80eaf220ee36a099faa
Joe Schmitt <joe@compass.com>
2017-01-18 16:10:44 -0500

    Formatting, grammar, and standardized documentation fixes

--- src/angularComponent/ngcOmniboxComponent.js
+++ src/angularComponent/ngcOmniboxComponent.js
@@ -5,33 +5,35 @@ import NgcOmniboxController from './ngcOmniboxController.js';
  *
  * The Omnibox Component is the main container for the Omnibox. It provides the following bindings:
  *
- * - source({query, suggestions}) {Promise}: This function populates the list of suggestions by
- *       returning a Promise that resolves to an Array of suggestions. The individual suggestions
- *       can be of any type, but the list of suggestions must be an array. If you wish to provide
- *       nested suggestions (such as category headers, or a tree structure) then you must provide a
- *       key called `children` which will then be looped through to find more children, or the end
- *       of the list. It receives, in its scope, access to a String called `query` which the current
- *       query in the input field, and an Array called `suggestions`, which is the current list
- *       of suggestions being displayed.
- * - ngModel {Any}: This is a one-way binding to the ngModel for the Omnibox. When the `multiple`
- *       option is set to true, then the ngModel should be an Array. Each item in the Array will be
- *       populated with choices from the objects passed via the source function. If the `multiple`
- *       option is false (default), then the ngModel will be set to the singular chosen suggestion.
- * - ngDisabled() {Boolean}: This function should return a boolean that determines if the
+ * - `source({query, suggestions}) {Promise}` _(Required)_: An expression that populates the list of
+ *       suggestions by returning a `Promise` that resolves to an array of suggestions. The
+ *       individual suggestions can be of any type, but the list of suggestions must be an array. If
+ *       you wish to provide nested suggestions (such as category headers, or a tree structure) then
+ *       you must provide a key called `children` on your suggestion which will then be looped
+ *       through to find more children, or the end of the list. It receives, in its scope, access to
+ *       a string called `query` with the current query in the input field, and an array called
+ *       `suggestions`, which is the current list of suggestions being displayed.
+ * - `ngModel {Any}` _(Required)_: This is a one-way binding to the ngModel for the Omnibox. When
+ *       the `multiple` option is set to `true`, then the `ngModel` should be an array. Each item in
+ *       the array will be populated with choices from the objects passed via the `source` function.
+ *       If the `multiple` option is `false` (default), then the `ngModel` will be set to the
+ *       singular chosen suggestion.
+ * - `ngDisabled() {Boolean}`: This expression should evaluate to a boolean that determines if the
  *       Omnibox component should be disabled.
- * - multiple {Boolean}: Whether to allow multiple choices from the list of suggestions. This option
- *       controls whether the ngModel will be an Array (multiple is on) or a single choice (off).
- * - hideOnBlur {Boolean}: Whether the list of suggestions should automatically hide when the
+ * - `multiple {Boolean}`: Whether to allow multiple choices from the list of suggestions. This
+ *       option
+ *     controls whether the `ngModel` will be an array (multiple is on) or a single choice (off).
+ * - `hideOnBlur {Boolean}`: Whether the list of suggestions should automatically hide when the
  *       component itself loses focus. Hitting ESC will always close the list of suggestions.
- * - isSelectable({suggestion}) {Boolean}: A function that should return a Boolean that determines
- *       if a suggestion is able to be interacted with. This function will be called whenever a
- *       suggestion is attempted to be highlighted either by the keyboard or mouse. It receives, in
- *       its scope, access to an object called `suggestion` which is the current sugestion that is
- *       being interacted with. A non-selectable suggestion cannot be clicked on, hovered over, or
- *       interacted with via the keyboard.
- * - canShowSuggestions({query}) {Boolean}: A function that should return a Boolean that
+ * - `isSelectable({suggestion}) {Boolean}`: An expression that should evaluate to a Boolean that
+ *       determines if a suggestion is able to be interacted with. This expression will be executed
+ *       whenever a suggestion is attempted to be highlighted either by the keyboard or mouse. It
+ *       receives, in its scope, access to an object called `suggestion` which is the current \
+ *       suggestion that is being interacted with. A non-selectable suggestion cannot be clicked on,
+ *       hovered over, or interacted with via the keyboard.
+ * - `canShowSuggestions({query}) {Boolean}`: An expression that should evaluate to a Boolean that
  *       determines whether or not the list of suggestions can be displayed. It receives, in its
- *       scope, access to a String called `query` which is the current query being searched on.
+ *       scope, access to a string called `query` which is the current query being searched on.
  *
  * The component has no template, all content that does not map to one of the sub-components will
  * be displayed in the final output as-is and un-modified.
