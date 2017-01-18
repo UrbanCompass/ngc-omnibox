66c6f4be19e21e92b726a0493a526fc0213cef28
Joe Schmitt <joe@compass.com>
2017-01-18 17:56:56 -0500

    Fix IE 11 IndexSizeError

--- src/angularComponent/ngcOmniboxController.js
+++ src/angularComponent/ngcOmniboxController.js
@@ -70,7 +70,7 @@ export default class NgcOmniboxController {
     // Remove the focus ring when the overall component is focused
     const styleSheets = this.doc.styleSheets;
     if (styleSheets && styleSheets.length) {
-      styleSheets[styleSheets.length - 1].insertRule('ngc-omnibox:focus {outline: none}');
+      styleSheets[styleSheets.length - 1].insertRule('ngc-omnibox:focus {outline: none}', 0);
     }
   }
 
