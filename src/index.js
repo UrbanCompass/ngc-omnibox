3c5ce7bc973868cf7709e543b874c0238489714e
Joe Schmitt <joe@compass.com>
2017-01-31 14:49:57 -0500

    Add filter for suggestion match highlighting
    Fixes #42

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import angular from 'angular';
 
 import ngcOmniboxComponent from './angularComponent/ngcOmniboxComponent.js';
-import ngcOmniboxFieldDirective from './angularComponent/ngcOmniboxFieldDirective.js';
+import ngcOmniboxHighlightMatchFilter from './angularComponent/ngcOmniboxHighlightMatchFilter.js';
 import ngcModifySuggestionsTemplateFactory from './angularComponent/ngcModifySuggestionsTemplateFactory.js';
+import ngcOmniboxFieldDirective from './angularComponent/ngcOmniboxFieldDirective.js';
 import ngcOmniboxSuggestionsDirective from './angularComponent/ngcOmniboxSuggestionsDirective.js';
 import ngcOmniboxSuggestionItemDirective from './angularComponent/ngcOmniboxSuggestionItemDirective.js';
 import ngcOmniboxChoicesDirective from './angularComponent/ngcOmniboxChoicesDirective.js';
 
 export default angular.module('ngc.omnibox', [])
   .component('ngcOmnibox', ngcOmniboxComponent)
-  .directive('ngcOmniboxField', ngcOmniboxFieldDirective)
   .factory('ngcModifySuggestionsTemplate', ngcModifySuggestionsTemplateFactory)
+  .filter('ngcOmniboxHighlightMatch', ngcOmniboxHighlightMatchFilter)
+  .directive('ngcOmniboxField', ngcOmniboxFieldDirective)
   .directive('ngcOmniboxSuggestions', ngcOmniboxSuggestionsDirective)
   .directive('ngcOmniboxSuggestionHeader', ngcOmniboxSuggestionItemDirective)
   .directive('ngcOmniboxSuggestionItem', ngcOmniboxSuggestionItemDirective)
